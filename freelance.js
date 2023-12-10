7b06f43ad6272d7a02cc58fdad537c3098eab58d
Jacqueline Guthart <146400160+jacquelineguthart@users.noreply.github.com>
2023-12-10 16:10:18 -0500

    Updating Homework

--- freelance.js
+++ freelance.js
@@ -16,10 +16,67 @@ const freelancers = [
       },
   ];
 
-// `setInterval` will call `addShape` every 1000 milliseconds (1 second)
-// and return an interval ID that we can use to stop the interval later.
-// Calling `clearInterval(addShapeIntervalId)` will stop the interval.
-const addShapeIntervalId = setInterval(addShape, 2000);
+//Need a random name
+
+const names = ["Jeremy", "Tyra", "Janice", "George"];
+
+
+function randoName () {
+  const index = Math.random()*names.length;
+  const indexfloor = Math.floor(index);
+  const chooseName = names[indexfloor];
+  return chooseName;
+}
+
+//Need to display names
+//Need to calculate average price
+//Need to display average price
+
+
+const occupations = ["Developer", "Account Manager", "Fireman","Police", "Winetaster"];
+
+let totalprice = 0
+let totalitems = 0
+
+
+function addFreelancer (X){
+
+ let price = Math.random()*100; 
+ totalprice = totalprice + price;
+ totalitems = totalitems + 1
+ const average = totalprice/totalitems;
+ document.getElementById('average').textContent= `Average: ${average.toFixed(0)}`
+ const A= randomthing (names);
+ const B= randomthing (occupations);
+ console.log(`Name: ${A}, Occupation: ${B}, Price: ${price.toFixed(2)}`);
+ const list = document.getElementById('list');
+ const listitem = document.createElement('li');
+ listitem.textContent = `Name: ${A}, Occupation: ${B}, Price: ${price.toFixed(2)}`
+ list.appendChild(listitem);
+ console.log("Example",list);
+}
+
+
+function randoOccupations (){
+  const index = Math.random()*occupations.length;
+  const indexfloor = Math.floor(index);
+  const chooseOccupation = occupations[indexfloor];
+
+  return chooseOccupation;
+
+}
+
+function randomthing (array){
+
+  const index = Math.random()*array.length;
+  const indexfloor = Math.floor(index);
+  const chooseOccupation = array[indexfloor];
+
+  return chooseOccupation;
+
+}
+
+const freelancerlooper = setInterval(addFreelancer, 2000);
 
 render(); // We call this function once to render the initial state
 
